aeb57c4d98e3015cd54c1b6b4fa0a0e4cc26eef3
Chloe <55222885+chloec222@users.noreply.github.com>
2020-01-11 23:43:58 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".thumbnail-item").click(function(event){
+    event.preventDefault();
+    });
+
+
+
+
+
+
+var imageLink = document.querySelector(".thumbnail-item");
+var thumbnailImage = document.querySelector(".thumbnail-image");
+var expandedViewOfSelectedImage = document.querySelector(".detail-image");
+
+
+// function setDetails(imageUrl){
+//     var detailImage = document.querySelector(detailImageSelector);
+//     detailImage.setAttribute("src",imageUrl);
+// }
+// setDetails("assets/images/rocketp.jpg");
+
+
+});
